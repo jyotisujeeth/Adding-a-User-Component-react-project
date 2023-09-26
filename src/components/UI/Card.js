8b7a2d0a50aa24b350677135af12977b86c63821
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-26 14:14:26 +0530

    card  components

---
+++ src/components/UI/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+import calsses from 'Card.module.css';
+const Card = (props) => {
+return
+(
+     <div className ={calsses.card}> (props.children)
+
+</div>;
+)};
+
+export default Card;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,21 @@
 import React from "react";
 
+import card from '../UI/Card';
 const AddUser = (props) => {
     const addUserHandler =(event) => {
     event.preventDefault();
     };
     
     return (
-        
+      <Card>
         <form onSubmit={addUserHandler}>
-            <label htmlFor="username"> UserName</label>
-            <input id="username" type=" text" ></input>
-            <label htmlFor="age">Age(year)</label>
-            <input id="age" type="number"></input>
-            <button type="submit">Add User</button>
+          <label htmlFor="username"> UserName</label>
+          <input id="username" type=" text"></input>
+          <label htmlFor="age">Age(year)</label>
+          <input id="age" type="number"></input>
+          <button type="submit">Add User</button>
         </form>
+      </Card>
     );
 };
 
